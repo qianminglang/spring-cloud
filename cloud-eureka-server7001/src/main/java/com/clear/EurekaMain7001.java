1b9e6c461688a98c45ee87b6014849dd4c6c3d8e
qml <asiainfo.com>
2020-11-03 22:48:44 +0800

    eureka集群搭建

---
+++ cloud-eureka-server7001/src/main/java/com/clear/EurekaMain7001.java
@@ -0,0 +1,21 @@
+package com.clear;
+
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * ClassName EurekaMain7001
+ *
+ * @author qml
+ * Date 2020/11/3 0003 21:50
+ * Version 1.0
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
--- cloud-eureka-server7002/src/main/java/com/clear/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/clear/EurekaMain7002.java
@@ -2,19 +2,19 @@ package com.clear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * ClassName Payment
+ * ClassName main
  *
  * @author qml
- * Date 2020/10/31 0031 15:00
+ * Date 2020/11/3 0003 22:39
  * Version 1.0
  **/
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentApplication {
+@EnableEurekaServer
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentApplication.class);
+        SpringApplication.run(EurekaMain7002.class,args);
     }
 }
